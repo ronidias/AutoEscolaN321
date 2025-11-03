db499dfbe76ebf9a812fb008485667cb667a3713
Roni <diasdossantosroni@gmail.com>
2025-11-03 19:58:50 -0300

    ResponseEntity

--- src/main/java/br/com/senai/autoescolan321/controller/InstrutorController.java
+++ src/main/java/br/com/senai/autoescolan321/controller/InstrutorController.java
@@ -2,6 +2,7 @@ package br.com.senai.autoescolan321.controller;
 
 import br.com.senai.autoescolan321.model.dto.instrutor.DadosAtualizacaoInstrutor;
 import br.com.senai.autoescolan321.model.dto.instrutor.DadosCadastroInstrutor;
+import br.com.senai.autoescolan321.model.dto.instrutor.DadosDetalhamentoInstrutor;
 import br.com.senai.autoescolan321.model.dto.instrutor.DadosListagemInstrutor;
 import br.com.senai.autoescolan321.entity.Instrutor;
 import br.com.senai.autoescolan321.repository.InstrutorRepository;
@@ -11,7 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/instrutores")
@@ -22,26 +27,36 @@ public class InstrutorController {
 
     @PostMapping
     @Transactional
-    public void cadastrarInstrutor(@RequestBody @Valid DadosCadastroInstrutor dados) {
+    public ResponseEntity <DadosDetalhamentoInstrutor> cadastrarInstrutor(
+            @RequestBody @Valid DadosCadastroInstrutor dados,
+            UriComponentsBuilder uriBuilder
+    ) {
+        Instrutor instrutor = new Instrutor(dados);
         instrutorRepository.save(new Instrutor(dados));
+        URI uri = uriBuilder.path("/instrutores/{id}")
+                .buildAndExpand(instrutor.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoInstrutor(instrutor));
     }
 
     @GetMapping
-    public Page<DadosListagemInstrutor> listarInstrutores(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return instrutorRepository.findAllByAtivoTrue(paginacao).map(DadosListagemInstrutor::new);
+    public ResponseEntity <Page<DadosListagemInstrutor>> listarInstrutores(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        Page page = instrutorRepository.findAllByAtivoTrue(paginacao).map(DadosListagemInstrutor::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizarInstrutor(@RequestBody @Valid DadosAtualizacaoInstrutor dados) {
+    public ResponseEntity <DadosDetalhamentoInstrutor> atualizarInstrutor(@RequestBody @Valid DadosAtualizacaoInstrutor dados) {
         Instrutor instrutor = instrutorRepository.getReferenceById(dados.id());
         instrutor.atualizarInformacoes(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoInstrutor(instrutor));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluirInstrutor(@PathVariable Long id) {
+    public ResponseEntity <Void> excluirInstrutor(@PathVariable Long id) {
         Instrutor instrutor = instrutorRepository.getReferenceById(id);
         instrutor.excluir();
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/br/com/senai/autoescolan321/model/dto/instrutor/DadosDetalhamentoInstrutor.java
@@ -0,0 +1,34 @@
+package br.com.senai.autoescolan321.model.dto.instrutor;
+
+
+import br.com.senai.autoescolan321.entity.Instrutor;
+import br.com.senai.autoescolan321.enumeration.Especialidade;
+import br.com.senai.autoescolan321.model.Endereco;
+
+public record DadosDetalhamentoInstrutor(
+
+
+
+        Long id,
+        Boolean ativo,
+        String nome,
+        String email,
+        String telefone,
+        String cnh,
+        Especialidade especialidade,
+        Endereco endereco)
+
+{
+
+    public DadosDetalhamentoInstrutor(Instrutor instrutor) {
+        this(
+                instrutor.getId(),
+                instrutor.getAtivo(),
+                instrutor.getNome(),
+                instrutor.getEmail(),
+                instrutor.getTelefone(),
+                instrutor.getCnh(),
+                instrutor.getEspecialidade(),
+                instrutor.getEndereco());
+    }
+}
